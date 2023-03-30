692f9da402eefc2abe01453da231a045800a2c46
arektar <zander5@mail.ru>
2023-03-30 20:35:37 +0300

    Added content changing with orientation.

--- app/src/main/java/com/example/ktsreddit/presentation/ui/pages/Onboarding.kt
+++ app/src/main/java/com/example/ktsreddit/presentation/ui/pages/Onboarding.kt
@@ -1,15 +1,14 @@
 package com.example.ktsreddit.presentation.ui.pages
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,23 +19,45 @@ import com.example.ktsreddit.presentation.ui.theme.KtsRedditTheme
 
 @Composable
 fun Onboarding() {
+    val configuration = LocalConfiguration.current
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Content()
+            }
+        }
+        else -> {
+            Column(
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Content()
+            }
+        }
+    }
+}
+
+@Composable
+fun Content() {
     Column(
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
     ) {
-
-        Text(text = stringResource(id = R.string.start_screen_hello),  fontSize = 25.sp)
+        Text(text = stringResource(id = R.string.start_screen_hello), fontSize = 25.sp)
         Image(
             imageVector = ImageVector.vectorResource(id = R.drawable._101381_light),
             contentDescription = "Lotus"
         )
-
-        Button(onClick = { /*TODO*/ }) {
-            Text(stringResource(id = R.string.start_screen_next), fontSize = 25.sp)
-        }
     }
 
+    Button(onClick = { /*TODO*/ }) {
+        Text(stringResource(id = R.string.start_screen_next), fontSize = 25.sp)
+    }
 }
 
 @Preview(showBackground = true)
